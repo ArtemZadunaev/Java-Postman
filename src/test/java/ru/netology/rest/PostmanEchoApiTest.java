5e7dc4c34d7d1a707db4c18605905cf8b616c51f
Erizard <Azgarot22@gmail.com>
2025-10-22 12:17:28 +0300

    fix mistake

--- src/test/java/ru/netology/rest/PostmanEchoApiTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoApiTest.java
@@ -17,7 +17,7 @@ public class PostmanEchoApiTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("{\"name\":\"Вася\", \"age\":42, \"height\": 183}"));
+                .body("data", equalTo("{\"name\":\"Вася\", \"age\":42, \"height\": 182}"));
     }
 
 
